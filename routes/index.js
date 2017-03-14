17d364c349f918e5f19ab02dba29ea5303adc4e0
Rahul Trehan <rtrehan26@gmail.com>
2017-03-13 23:52:36 -0700

    adding sequelize database support and adding sample user model

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
+var models  = require('../models');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+  	title: 'Express',
+  	users: users
+  	});
 });
 
 module.exports = router;
