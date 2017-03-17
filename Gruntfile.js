db1e9afd70d498ff3aad8922fb995d4add658a98
Rahul Trehan <rtrehan26@gmail.com>
2017-03-16 20:25:13 -0700

    adding grunt packaging to project

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+
+/*Grunt task file */
+
+module.exports = grunt => {
+
+	//load all tasks defined in package.json.
+	require('load-grunt-tasks')(grunt);
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+	        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+	      },
+	      build: {
+	        src: 'src/<%= pkg.name %>.js',
+	        dest: 'build/<%= pkg.name %>.min.js'
+	      }
+		}
+
+	});
+
+	grunt.registerTask('default', ['uglify']);
+}
